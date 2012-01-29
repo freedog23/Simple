bfd153ffe2ea90ffe49f39f10aeea13d89545531
beerofeel <beerofeel@gmail.com>
2012-01-29 02:33:31 +0200

    add

--- src/PetMap.java
+++ src/PetMap.java
@@ -12,13 +12,17 @@ import java.util.Map;
  */
 public class PetMap {
     public static void main(String[] args) {
+
         Map<String, Pet> petMap = new HashMap<String, Pet>();
+
         petMap.put("My cat", new Cat("Molly"));
         petMap.put("My dog", new Dog("Ginger"));
         petMap.put("My Hamster", new Hamster("Bosco"));
         
         System.out.println(petMap);
+
         Pet dog = petMap.get("My dog");
+
         System.out.println(dog);
         System.out.println(petMap.containsKey("My dog"));
         System.out.println(petMap.containsValue(dog));
