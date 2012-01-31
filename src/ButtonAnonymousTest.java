0d6ef018d665e73dd5788841c28d5b2806128e6d
Hazarti Lirot <hazartilirot@gmail.com>
2012-01-31 02:18:46 +0200

    delete comments

--- src/ButtonAnonymousTest.java
+++ src/ButtonAnonymousTest.java
@@ -5,11 +5,11 @@ import java.awt.event.ActionListener;
 
 class ButtonAnonymousPanel extends JPanel {
     
-    public void makeButton(String name, final Color color) {
+    void makeButton(String name, final Color color) {
         JButton button = new JButton(name);
         add(button);
-
         button.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 setBackground(color);
             }
@@ -24,46 +24,13 @@ class ButtonAnonymousPanel extends JPanel {
 }
 public class ButtonAnonymousTest {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("ButtonAnonymousTest");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setTitle("ButtonAnonymousTest");
         frame.setLocation(screenSize.width / 4, screenSize.height / 4);
         frame.setSize(screenSize.width / 2, screenSize.height / 2);
         frame.add(new ButtonAnonymousPanel());
-        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-class ButtonAnonymousPanel extends JPanel {
-
-    private void makeButton(String name, final Color color) {
-        JButton button = new JButton(name);
-        add(button);
-
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setBackground(color);
-            }
-        });
-    }
-
-    ButtonAnonymousPanel() {
-        makeButton("yellow", Color.YELLOW);
-        makeButton("blue", Color.BLUE);
-        makeButton("red", Color.RED);
-    }
-}
-*/
