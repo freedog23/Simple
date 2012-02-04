a2464d70f2bfe6c7078d6a7bd8373da4dcbd0284
Hazarti Lirot <hazartilirot@gmail.com>
2012-02-04 04:27:20 +0200

    new classes

--- src/Patterns/SimUDuck/Duck.java
+++ src/Patterns/SimUDuck/Duck.java
@@ -14,22 +14,38 @@ interface QuackBehavior {
     void quack();
 }
 
-
 public abstract class Duck {
+    
+    protected FlyBehavior flyBehavior;
+    protected QuackBehavior quackBehavior;
     
     public abstract void display();
     
     public void swim() { System.out.println("All ducks float even decoys"); }
+
+    public void performFly() { flyBehavior.fly(); }         //methods
+    public void performQuack() { quackBehavior.quack(); }   // of delegation
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+    public void setQuackBehavior(FlyBehavior flyBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
 }
+
 class DucksWings implements FlyBehavior {
     public void fly() { System.out.println("I'm flying!"); }
 }
 class DucksNoWings implements FlyBehavior {
-    public void fly() { System.out.print("I'm can't fly."); }
+    public void fly() { System.out.println("I'm can't fly."); }
 }
 class DucksQuack implements QuackBehavior {
     public void quack() { System.out.println("quack-quack");}
 }
 class DuckSqueak implements QuackBehavior {
     public void quack() { System.out.println("squeak-squeak");}
+}
+class DuckMute implements QuackBehavior {
+    public void quack() {} // can't talk
 }
