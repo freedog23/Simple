bd592560e6b66343d71e5f839d62e70f3d0b72aa
beerofeel <beerofeel@gmail.com>
2012-02-04 20:55:56 +0200

    add info

---
+++ src/Patterns/SimUDuck/DuckManok.java
@@ -0,0 +1,12 @@
+package Patterns.SimUDuck;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ASUS
+ * Date: 04.02.12
+ * Time: 20:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class DuckManok implements QuackBehavior{
+    public void quack() { System.out.println("quack-quack"); }
+}
